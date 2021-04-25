7826a28cd4ef599f5ca1eb03d31b16afa5d20e73
SIUSWHY <60172736+SIUSWHY@users.noreply.github.com>
2021-04-25 22:31:15 +0300

    add content

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const cards = [
         interpreter: 'Шишмарева Мария Андреевна',
         editor: 'Иванова Виктория',
         genre: 'Зарубежная приключенческая литература и др.',
-        publishing_house: 'LostBook, 2021 г',
+        publishing_house: 'Лабиринт, 2021 г.',
         price: '496',
     }, {
         img: 'img/2.jpg',
@@ -15,7 +15,62 @@ const cards = [
         genre: 'Современная зарубежная проза',
         publishing_house: 'АСТ, 2021 г.',
         price: '711',
+    }, {
+        img: 'img/3.jpg',
+        author: 'Мейер Марисса',
+        interpreter: 'Колябина Е. И., Шолохова Елена',
+        editor: '-',
+        genre: 'Молодежная литература и др.',
+        publishing_house: 'АСТ, 2021 г.',
+        price: '541',
+    }, {
+        img: 'img/4.png',
+        author: 'Верн Жюль',
+        interpreter: 'Бекетова А.',
+        editor: 'Иванова Виктория',
+        genre: 'Зарубежная приключенческая литература и др.',
+        publishing_house: 'Лабиринт, 2021 г.',
+        price: '557',
+    }, {
+        img: 'img/5.png',
+        author: 'Ли Стейси',
+        interpreter: 'Нефедова Анна',
+        editor: '-',
+        genre: 'Молодежная литература',
+        publishing_house: ' Аркадия, 2021 г.',
+        price: '912',
+    }, {
+        img: 'img/6.jpg',
+        author: 'Файзал Хафса',
+        interpreter: '-',
+        editor: '-',
+        genre: 'Молодежная литература, Современная зарубежная проза',
+        publishing_house: ' Эксмо, 2021 г.',
+        price: '331',
+    }, {
+        img: 'img/7.jpg',
+        author: 'Лавкрафт Говард Филлипс',
+        interpreter: 'Брилова Л., Мичковский О., Дорогокупля В. Н.',
+        editor: '-',
+        genre: 'Классическая зарубежная проза',
+        publishing_house: 'Феникс, 2021 г.',
+        price: '609',
+    }, {
+        img: 'img/8.jpg',
+        author: 'Лукьяненко Сергей Васильевич',
+        interpreter: '-',
+        editor: '-',
+        genre: 'Современная отечественная проза',
+        publishing_house: 'АСТ, 2021 г.',
+        price: '620',
     },
+
+
+    // author - Автор
+    // interpreter - Переводчик
+    // editor-Редактор
+    // genre-Жанр
+    // publishing_house-Издательство
 
 ]
 
@@ -26,6 +81,7 @@ for (let i = 0; i < cards.length; i++) {
     <div class="holder">
             <img class="style_poster_book" src="${cards[i].img}">
             <div class="block">
+                <div class="max_size_text_block">
                 <div class="discrip_text_style">Автор: <div class="input_discrip_text_style">${cards[i].author}</div>
                 </div>
                 <div class="discrip_text_style">Переводчик: <div class="input_discrip_text_style">${cards[i].interpreter}</div>
@@ -36,6 +92,7 @@ for (let i = 0; i < cards.length; i++) {
                 </div>
                 <div class="discrip_text_style">Издательство: <div class="input_discrip_text_style">${cards[i].publishing_house}
                     </div>
+                </div>
                 </div>
                 <div class="buy_button_style">
                     <div class="price_text_style">Цена: ${cards[i].price} р.</div>
